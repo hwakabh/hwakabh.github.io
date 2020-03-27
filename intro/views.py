0d6676492f5c5f34ea2d9978ab8527fe983614f1
hwakabh <hrykwkbys1024@gmail.com>
2020-03-27 12:05:56 +0900

    Added templates and integrated with functional views.

--- intro/views.py
+++ intro/views.py
@@ -1,38 +1,57 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+HTML = 'intro/index.html'
 
 # Landing page
 def home(request):
     # [Home]icons, main-photo, external-links(GitHub, LinkedIn, Facebook, Instagram ...etc)
-    return HttpResponse('Hello Django')
+    message = 'This is Home section'
+    return render(request, HTML, {
+        'msg': message,
+    })
 
 
 # Introduction of myself
 def bio(request):
     # [about|biography]skill-map, career, certs, educations as static information
-    return HttpResponse('BIO')
+    message = 'This is bio section'
+    return render(request, HTML, {
+        'msg': message,
+    })
 
 
 # Business side projects
 def works(request):
     # [projects]Get projects data from LinkedIn API and render to django template
-    return HttpResponse('WORKS')
+    message = 'This is works section'
+    return render(request, HTML, {
+        'msg': message,
+    })
 
 
 # Non-business side projects
 def scraps(request):
     # [personal-works]Links to homepages created, Songs and media of SonicPi
     # CRUD Implementation with Django models
-    return HttpResponse('SCRAPS')
+    message = 'This is scraps section'
+    return render(request, HTML, {
+        'msg': message,
+    })
 
 
 # Personal blogs
 def blogs(request):
     # [monologue]Fetch data from medium API and parse contents with rendering to template
-    return HttpResponse('BLOGS')
+    message = 'This is blogs section'
+    return render(request, HTML, {
+        'msg': message,
+    })
 
 
 # Mail forms or contacts to my gmail address
 def contacts(request):
-    return HttpResponse('CONTACTS')
+    message = 'This is contact section'
+    return render(request, HTML, {
+        'msg': message,
+    })
