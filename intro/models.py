1758346ea43984fa25226b9addcde64004fcaea6
hwakabh <hrykwkbys1024@gmail.com>
2020-04-09 09:20:31 +0900

    #12 Re-migrated the model since it contains customer information, and added initial fixture data.

--- intro/models.py
+++ intro/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 class Project(models.Model):
     name = models.CharField(max_length=100)
-    customer_name = models.CharField(max_length=30)
     start = models.DateField()
     end = models.DateField()
     description = models.TextField()
