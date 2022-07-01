95f8501338ec6759efb030265dfb87d9ea40820e
hwakabh <hrykwkbys1024@gmail.com>
2022-07-02 02:59:30 +0900

    Updated backend src.

--- api-server/routes/index.js
+++ api-server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'express-backend' });
+  res.render('index', { title: 'express-rest-api-server' });
 });
 
 module.exports = router;
