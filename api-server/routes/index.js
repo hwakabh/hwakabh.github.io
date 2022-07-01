c9d0df2433c44ebe77f1b9c669a523a13fa61813
hwakabh <hrykwkbys1024@gmail.com>
2022-07-01 22:23:11 +0900

    Updated src both backend and frontend for checking gh-actions.

--- api-server/routes/index.js
+++ api-server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'hwakabh-api-server' });
+  res.render('index', { title: 'express-backend' });
 });
 
 module.exports = router;
