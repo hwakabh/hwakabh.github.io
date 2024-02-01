f2d48736fa0d455fc8ad15cf4ac1b9a7ff50345e
hwakabh <hrykwkbys1024@gmail.com>
2024-02-02 03:05:08 +0900

    feat: added endpoints for health checks.

--- api-server/routes/index.js
+++ api-server/routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'express-rest-api-server' });
+router.get('/', function(req, res) {
+  res.json({
+    "path": req.path,
+    "body": "Hello, Developers!"
+  });
+});
+
+// Endpoint of probe target
+router.get('/healthz', function(req, res) {
+  res.status(200).json({
+    "status": "ok"
+  });
 });
 
 module.exports = router;
