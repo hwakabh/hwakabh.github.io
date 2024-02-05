2c6e0d78c43102f0cb054ab3b2494dd66643013d
hwakabh <hrykwkbys1024@gmail.com>
2024-02-06 03:46:24 +0900

    feat(ui): enabled to invoke backend and render them to templates.

--- web-frontend/src/components/Works.vue
+++ web-frontend/src/components/Works.vue
@@ -1,34 +1,41 @@
 <script setup>
 import axios from "axios";
-import { onMounted } from "vue";
+import { ref, onMounted } from "vue";
 
 
 // axios.defaults.baseURL = 'http://localhost:3000';
-const url = "https://jsonplaceholder.typicode.com/users";
-// const url = "http://localhost:3000/projects";
+// const url = "https://jsonplaceholder.typicode.com/users";
+const url = "http://localhost:3000/projects";
 
-// Since this is not the reactive data, need not to use `ref()`
+const projectData = ref('');
+
 const projects = axios.get(url)
   .then((resp) => {
-    console.log("Promise complete");
-    return resp.data
+    console.log(resp.data.data);
+    console.log(resp.data.data[0].title);
+    projectData.value = resp.data.data[0].descriptions[0];
+    // console.log("Promise complete");
+    // return resp.data
   })
   .catch((err) => {
-    console.log("Promise broken");
+    // console.log("Promise broken");
     console.log(err);
     return null
   })
 
+
 // Vue lifecycle hooks
 onMounted(function() {
-  console.log(projects);
+  // console.log(projects.path)
+  // console.log(projects.data)
+  console.log(projectData);
 })
 
 </script>
 
 <template>
   <p>
-    Here is my works !
-    {{ projects }}
+    Here is my works!
+    {{ projectData }}
   </p>
 </template>
