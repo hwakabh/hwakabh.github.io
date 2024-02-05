157027e35800438d343ca8641d01bbe025c190c3
hwakabh <hrykwkbys1024@gmail.com>
2024-02-06 02:56:33 +0900

    feat(axios): enabled to call external endpoint.

--- web-frontend/src/components/Works.vue
+++ web-frontend/src/components/Works.vue
@@ -1,5 +1,34 @@
+<script setup>
+import axios from "axios";
+import { onMounted } from "vue";
+
+
+// axios.defaults.baseURL = 'http://localhost:3000';
+const url = "https://jsonplaceholder.typicode.com/users";
+// const url = "http://localhost:3000/projects";
+
+// Since this is not the reactive data, need not to use `ref()`
+const projects = axios.get(url)
+  .then((resp) => {
+    console.log("Promise complete");
+    return resp.data
+  })
+  .catch((err) => {
+    console.log("Promise broken");
+    console.log(err);
+    return null
+  })
+
+// Vue lifecycle hooks
+onMounted(function() {
+  console.log(projects);
+})
+
+</script>
+
 <template>
   <p>
     Here is my works !
+    {{ projects }}
   </p>
 </template>
