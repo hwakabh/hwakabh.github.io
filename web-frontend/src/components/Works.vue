8d061b4a0a4003e395653ef06ec53e75a4fb18e3
hwakabh <hrykwkbys1024@gmail.com>
2024-02-07 13:38:13 +0900

    feat(ui): connected to backend in /about and /works.

--- web-frontend/src/components/Works.vue
+++ web-frontend/src/components/Works.vue
@@ -4,18 +4,18 @@ import { ref, onMounted } from "vue";
 
 
 // axios.defaults.baseURL = 'http://localhost:3000';
-// const url = "https://jsonplaceholder.typicode.com/users";
 const url = "http://localhost:3000/api/v1/cv/projects";
 
 const projectData = ref('');
 axios.get(url)
   .then((resp) => {
-    // console.log(resp.data.projects[0].title);
-    projectData.value = resp.data.projects[0].descriptions[0];
+    console.log(resp.data)
+    projectData.value = resp.data.content[0].descriptions[0];
   })
   .catch((err) => {
     console.log(err);
   })
+
 
 // Vue lifecycle hooks
 onMounted(function() {
