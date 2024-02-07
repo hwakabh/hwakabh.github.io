13097c88db2802e034319998e364c2c57ec968ed
hwakabh <hrykwkbys1024@gmail.com>
2024-02-07 12:36:46 +0900

    chore: added notes and made using const instead of var.

--- api-server/routes/index.js
+++ api-server/routes/index.js
@@ -1,6 +1,7 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
+// API root
 router.get('/', function(req, res) {
   res.json({
     "path": req.path,
