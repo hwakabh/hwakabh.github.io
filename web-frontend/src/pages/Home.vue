70748e4a22b4d39c60b4ebf0ee8c29a8a35b0db2
hwakabh <hrykwkbys1024@gmail.com>
2024-02-08 16:21:41 +0900

    ui: implemented vue-writer in top page.

--- web-frontend/src/pages/Home.vue
+++ web-frontend/src/pages/Home.vue
@@ -1,3 +1,38 @@
+<script setup>
+import { ref } from 'vue'
+import VueWriter from 'vue-writer'
+
+const words = ref([
+  "Welcome!",
+  "ようこそ！",
+  "欢迎！",
+  "환영합니다！",
+  "Bienvenue!",
+  "Benvenuto!",
+  "Willkommen!",
+  "¡Bienvenido!",
+  "Bem-vindo!",
+  "Ласкаво просимо!",
+  "ยินดีต้อนรับ!",
+  "Nau mai!",
+  "ברוכים הבאים!",
+])
+</script>
+
 <template>
-  <p>This is home</p>
+  <h1> Hi there! </h1>
+  <VueWriter
+    class="top-message"
+    :array="words"
+    :delay="1000"
+    :type-speed="80"
+    :erase-speed="80"
+     >
+  </VueWriter>
 </template>
+
+<style>
+.top-message {
+  font-size: 60pt;
+}
+</style>
