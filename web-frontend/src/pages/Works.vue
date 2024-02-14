a453311462a356ff8e53f337bd3aed0698b87308
hwakabh <hrykwkbys1024@gmail.com>
2024-02-14 14:31:27 +0900

    fix(api): enabled to fetch VITE_API_ENDPOINT.

--- web-frontend/src/pages/Works.vue
+++ web-frontend/src/pages/Works.vue
@@ -3,11 +3,8 @@ import axios from "axios";
 import { ref, onMounted } from "vue";
 
 
-// axios.defaults.baseURL = 'http://localhost:3000';
-const url = "http://localhost:3000/api/v1/cv/projects";
-
 const projectData = ref('');
-axios.get(url)
+axios.get('/api/v1/cv/projects')
   .then((resp) => {
     console.log(resp.data)
     projectData.value = resp.data.content[0].descriptions[0];
