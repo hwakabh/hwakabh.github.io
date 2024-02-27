8c448eeaf5c434c775824b488093ed53fbc5ea55
hwakabh <hrykwkbys1024@gmail.com>
2024-02-27 16:08:16 +0900

    feat(api): added contactRouter and implemented POST endpoint.

---
+++ api-server/routes/contact.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.post('/', (req, res, next) => {
+  // #swagger.tags = ['Contact']
+  // #swagger.description = 'invoke external API to send email'
+  console.log(req.body);
+  res.header('Content-Type', 'application/json; charset=utf-8');
+  res.json({
+    status: "POST ok"
+  })
+});
+
+router.get('/thanks', (req, res, next) => {
+  // #swagger.tags = ['Contact']
+  // #swagger.description = 'returns thanks message after POST requests'
+  res.header('Content-Type', 'application/json; charset=utf-8');
+  res.json({
+    "path": req.originalUrl,
+    "content": "Thank you for making contact!"
+  });
+})
+
+module.exports = router;
