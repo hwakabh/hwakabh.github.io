5b2803346d9e1f1fe2c012be41e02f6865d876da
hwakabh <hrykwkbys1024@gmail.com>
2024-02-28 23:55:02 +0900

    feat(ui): enabled to fetch backend data.

--- web-frontend/src/pages/Works.vue
+++ web-frontend/src/pages/Works.vue
@@ -2,12 +2,13 @@
 import axios from "axios";
 import { ref, onMounted } from "vue";
 
+import GitHubIcon from '../components/icons/GitHub.vue';
 
-const projectData = ref('');
-axios.get('/api/v1/cv/projects')
+const projectData = ref([]);
+axios.get('/api/v1/works')
   .then((resp) => {
-    console.log(resp.data)
-    projectData.value = resp.data.content[0].descriptions[0];
+    console.log(resp.data.content);
+    projectData.value = resp.data.content;
   })
   .catch((err) => {
     console.log(err);
@@ -23,7 +24,27 @@ onMounted(() => {
 
 <template>
   <p>
-    Here is my works!
-    {{ projectData }}
+    Personal Works
   </p>
+
+  <div v-for="(p, idx) in projectData"
+  :key="idx"
+  class="project-detail" >
+    <p>
+      <a :href="p.url" target="_blank"> {{ p.title }} </a>
+
+      <a v-if="p.gitHubRepoUrl" :href="p.gitHubRepoUrl" target="_blank">
+        <component :is="GitHubIcon" />
+      </a>
+      <br>
+
+      Descriptions: {{ p.descriptions }}
+      <br>
+      <!-- TODO: loop with v-for -->
+      Technical Stacks: {{ p.techStacks }}
+      <br>
+      {{ p.imageUrl }}
+    </p>
+  </div>
+
 </template>
