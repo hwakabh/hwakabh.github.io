72e7dca841f6b858471ff3f4a0d0ecfad37593ef
hwakabh <hrykwkbys1024@gmail.com>
2024-03-01 11:54:15 +0900

    feat(ui): implemented UI for education endpoint.

--- web-frontend/src/pages/Cv.vue
+++ web-frontend/src/pages/Cv.vue
@@ -11,6 +11,26 @@ axios.get('/api/v1/cv/projects')
   .catch((err) => {
     console.log(err);
   })
+
+const educationData = ref([]);
+axios.get('/api/v1/cv/educations')
+  .then((resp) => {
+    console.log(resp.data.content);
+    educationData.value = resp.data.content;
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+
+const publicationData = ref([]);
+axios.get('/api/v1/cv/publications')
+  .then((resp) => {
+    console.log(resp.data.content);
+    publicationData.value = resp.data.content;
+  })
+  .catch((err) => {
+    console.log(err);
+  })
 </script>
 
 <template>
@@ -32,4 +52,31 @@ axios.get('/api/v1/cv/projects')
       Skills: {{ p.skills }}
     </p>
   </div>
+
+  <h3>
+    Educations
+  </h3>
+  <div v-for="(e, idx) in educationData"
+  :key="idx"
+  class="education-detail" >
+    <p>
+      <h5> {{ e.title }} </h5>
+      <div> {{ e.descriptions }}</div>
+      <div> Period: {{ e.start }} - {{ e.end }} </div>
+    </p>
+  </div>
+
+  <h3>
+    Publications
+  </h3>
+  <div v-for="(pb, idx) in publicationData"
+  :key="idx"
+  class="publications-detail" >
+    <p>
+      <h5> {{ pb.title }} </h5>
+      <div> {{ pb.descriptions }}</div>
+      <div> Download Link(pdf): {{ pb.link }} </div>
+    </p>
+  </div>
+
 </template>
