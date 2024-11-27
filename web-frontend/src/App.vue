d1872810e6989d9c06d555b8ddebc84e8f046459
hwakabh <hrykwkbys1024@gmail.com>
2024-11-27 12:48:50 +0900

    refactor: refactored App.vue.

--- web-frontend/src/App.vue
+++ web-frontend/src/App.vue
@@ -1,8 +1,5 @@
 <script setup>
-// required for page scrolling
-// import { IonPage, IonContent } from '@ionic/vue';
-
-// import { IonCol, IonGrid, IonRow } from '@ionic/vue';
+import SidebarContent from './components/SidebarContent.vue';
 import FooterContent from './components/FooterContent.vue';
 
 </script>
@@ -16,83 +13,15 @@ import FooterContent from './components/FooterContent.vue';
 <script src="assets/js/util.js"></script>
 <script src="assets/js/main.js"></script> -->
 
-    <section id="sidebar">
-				<div class="inner">
-					<nav>
-						<ul>
-							<li><router-link to="/">Home</router-link> </li>
-							<li><router-link to="/about">About</router-link> </li>
-							<li><router-link to="/works">Works</router-link> </li>
-							<li><router-link to="/contact">Contact</router-link> </li>
-						</ul>
-					</nav>
-				</div>
-			</section>
+  <SidebarContent />
 
+  <div id="wrapper">
+    <router-view />
+  </div>
 
-		<!-- Wrapper -->
-    <div id="wrapper">
-      <router-view />
-    </div>
-
-  <!-- Footer -->
   <footer id="footer" class="wrapper style1-alt">
-  <FooterContent />
+    <FooterContent />
   </footer>
-
-
-  <!-- <ion-page>
-    <ion-content>
-
-      <header>
-        <ion-grid>
-          <ion-row>
-            <ion-col />
-            <ion-col size="8">
-              <div class="route-wrap">
-                <router-link to="/">Home</router-link>
-                |
-                <router-link to="/about">About me</router-link>
-                |
-                <router-link to="/cv">Resume</router-link>
-                |
-                <router-link to="/works">Side Gigs</router-link>
-            </div>
-            </ion-col>
-            <ion-col />
-          </ion-row>
-        </ion-grid>
-
-      </header>
-
-      <main>
-        <ion-grid>
-          <ion-row>
-            <ion-col />
-            <ion-col size="10">
-
-              <router-view />
-
-            </ion-col>
-            <ion-col />
-          </ion-row>
-        </ion-grid>
-      </main>
-
-      <footer>
-        <ion-grid>
-          <ion-row>
-            <ion-col />
-            <ion-col size="10">
-              <FooterContent />
-            </ion-col>
-            <ion-col />
-          </ion-row>
-        </ion-grid>
-      </footer>
-
-    </ion-content>
-  </ion-page> -->
 
 </template>
 
--- web-frontend/src/main.js
+++ web-frontend/src/main.js
@@ -1,26 +1,11 @@
 import { createApp } from 'vue';
 import { router } from './router/index';
-// import { IonicVue } from '@ionic/vue';
 
 import App from './App.vue';
 import axios from 'axios';
 
 import './assets/main.css';
-
-// /* Core CSS required for Ionic components to work properly */
-// import '@ionic/vue/css/core.css';
-// /* Basic CSS for apps built with Ionic */
-// import '@ionic/vue/css/normalize.css';
-// import '@ionic/vue/css/structure.css';
-// import '@ionic/vue/css/typography.css';
-// /* Optional CSS utils that can be commented out */
-// import '@ionic/vue/css/padding.css';
-// import '@ionic/vue/css/float-elements.css';
-// import '@ionic/vue/css/text-alignment.css';
-// import '@ionic/vue/css/text-transformation.css';
-// import '@ionic/vue/css/flex-utils.css';
-// import '@ionic/vue/css/display.css';
-
+import './assets/noscript.css';
 
 axios.defaults.baseURL = import.meta.env.VITE_API_ENDPOINT ?? 'http://localhost:3000';
 
