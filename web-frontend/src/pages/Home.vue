5de607047c6bdcb06a3d5a66956814ea1ec0407d
Hiroyuki Wakabayashi <25563897+hwakabh@users.noreply.github.com>
2025-04-03 13:35:23 +0900

    feat: frontend enhancement (#584) (#585)
    * chore: moved license texts into footer.
    
    * chore: updated href for blogs.
    
    * test: updated expected values.

--- web-frontend/src/pages/Home.vue
+++ web-frontend/src/pages/Home.vue
@@ -1,46 +1,47 @@
 <script setup>
-import { ref } from 'vue'
-import VueWriter from 'vue-writer'
-
+// import { ref } from 'vue'
+// import VueWriter from 'vue-writer'
 import SocialLink from '../components/SocialLink.vue'
 
-const words = ref([
-  "Welcome!",
-  "ようこそ！",
-  "欢迎！",
-  "환영합니다！",
-  "Bienvenue!",
-  "Benvenuto!",
-  "Willkommen!",
-  "¡Bienvenido!",
-  "Bem-vindo!",
-  "Ласкаво просимо!",
-  "ยินดีต้อนรับ!",
-  "Nau mai!",
-  "ברוכים הבאים!",
-])
+// const words = ref([
+//   "Welcome!",
+//   "ようこそ！",
+//   "欢迎！",
+//   "환영합니다！",
+//   "Bienvenue!",
+//   "Benvenuto!",
+//   "Willkommen!",
+//   "¡Bienvenido!",
+//   "Bem-vindo!",
+//   "Ласкаво просимо!",
+//   "ยินดีต้อนรับ!",
+//   "Nau mai!",
+//   "ברוכים הבאים!",
+// ])
 </script>
 
 <template>
 
-  <VueWriter
+  <!-- <VueWriter
     class="top-message"
     :array="words"
     :delay="1000"
     :type-speed="80"
     :erase-speed="80"
     >
-  </VueWriter>
+  </VueWriter> -->
 
   <section id="intro" class="wrapper style1 fullscreen fade-up">
     <div class="inner">
       <h1 class="top-message">Hi, there!</h1>
-      <p>Just another fine responsive site template designed by <a href="http://html5up.net">HTML5 UP</a><br />
-      and released for free under the <a href="http://html5up.net/license">Creative Commons</a>.</p>
+      <p>
+        Whether it is good or bad with your decisions, the results can be known only afterwards, <br />
+        the sky is the limit for your creativities !
+      </p>
       <SocialLink />
       <br>
       <ul class="actions">
-        <li><router-link to="/about" class="button scrolly">Learn more</router-link></li>
+        <li><router-link to="/about" class="button scrolly">About me</router-link></li>
       </ul>
     </div>
   </section>
