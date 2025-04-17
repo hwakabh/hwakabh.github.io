0e0e408211eb0e3b797165eb596d5f193b8631cc
Hiroyuki Wakabayashi <25563897+hwakabh@users.noreply.github.com>
2025-04-17 22:07:28 +0900

    chore: removed unused assets from code bases. (#596)

--- web-frontend/src/App.vue
+++ web-frontend/src/App.vue
@@ -5,13 +5,6 @@ import FooterContent from './components/FooterContent.vue';
 </script>
 
 <template>
-<!-- <script src="assets/js/jquery.min.js"></script>
-<script src="assets/js/jquery.scrollex.min.js"></script>
-<script src="assets/js/jquery.scrolly.min.js"></script>
-<script src="assets/js/browser.min.js"></script>
-<script src="assets/js/breakpoints.min.js"></script>
-<script src="assets/js/util.js"></script>
-<script src="assets/js/main.js"></script> -->
 
   <SidebarContent />
 
