4778eccf882d3dbccca6d43b55339da57fa50e2f
hwakabh <hrykwkbys1024@gmail.com>
2025-06-08 22:32:01 +0900

    feat(api): added API endpoint to load dotfile contents.

--- api-server/routes/api.js
+++ api-server/routes/api.js
@@ -5,6 +5,7 @@ const aboutRouter = require('./about');
 const cvRouter = require('./cv');
 const worksRouter = require('./works');
 const contactRouter = require('./contact');
+const configRouter = require('./config');
 
 const URL_PREFIX = '/api/v1';
 
@@ -12,5 +13,6 @@ router.use(URL_PREFIX + '/about', aboutRouter);
 router.use(URL_PREFIX + '/cv', cvRouter);
 router.use(URL_PREFIX + '/works', worksRouter);
 router.use(URL_PREFIX + '/contact', contactRouter);
+router.use(URL_PREFIX + '/_config', configRouter);
 
 module.exports = router;
---
+++ api-server/routes/config.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+
+
+router.get('/git', async (req, res, next) => {
+  // #swagger.tags = ['Config']
+  // #swagger.summary = 'Fetch raw data from hwakabh/dotfiles'
+  // #swagger.description = '/api/v1/_config'
+  const url = 'https://raw.githubusercontent.com/hwakabh/dotfiles/refs/heads/main/gitconf/.gitconfig';
+  const gitconfig = await axios.get(url)
+    .then(response => {
+      return response.data
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  res.header('Content-Type', 'application/json; charset=utf-8');
+  res.json({
+    "path": req.originalUrl,
+    "content": gitconfig
+  });
+
+});
+
+router.get('/bashrc', async (req, res, next) => {
+  // #swagger.tags = ['Config']
+  // #swagger.summary = 'Fetch raw data from hwakabh/dotfiles'
+  // #swagger.description = '/api/v1/_config'
+  const url = 'https://raw.githubusercontent.com/hwakabh/dotfiles/refs/heads/main/bash/.bashrc';
+  const bashrc = await axios.get(url)
+    .then(response => {
+      return response.data
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  res.header('Content-Type', 'application/json; charset=utf-8');
+  res.json({
+    "path": req.originalUrl,
+    "content": bashrc
+  });
+
+});
+
+
+module.exports = router;
