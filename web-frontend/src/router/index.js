245ac564807e575082adfb468b56ceff9956901f
hwakabh <hrykwkbys1024@gmail.com>
2025-07-28 15:40:16 +0900

    feat(vue-router): added scrollBehavior for navigations.

--- web-frontend/src/router/index.js
+++ web-frontend/src/router/index.js
@@ -17,4 +17,11 @@ const routes = [
 export const router = createRouter({
   history: createWebHistory(),
   routes: routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { top: 0 };
+    }
+  }
 });
